51e63171f333004e8c9cc5c9b801d55e9ab72a7b
Navse Julia <89183463550@mail.ru>
2024-01-21 05:13:38 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./index.html";
+import "./index.scss";
+
+import "./js/swiper";
+import "./js/sidebar-menu";
+import "./js/modal-window";
+import "./js/section-main__button";
+import "./js/section-brands__button";
+import "./js/section-type__button";
